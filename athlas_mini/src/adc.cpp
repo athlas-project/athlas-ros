443903fa792915bb12a03de536a7d00665c82706
athlas-project <athlas@ethz.ch>
2016-12-14 15:00:30 +0000

    CMakeLists working

--- athlas_mini/src/adc.cpp
+++ athlas_mini/src/adc.cpp
@@ -8,7 +8,7 @@
 //      gpio load i2c
 //before using
  
-#include "dockingstation/adc.hpp"
+#include "athlas_mini/adc.hpp"
 
 int fd;  /* I2C File descriptor */
 int ADS1015Addr = 0x48;  /* device address */
---
+++ athlas_mini/src/athlas_mini_node.cpp
@@ -0,0 +1,9 @@
+#include "athlas_mini/athlas_mini.hpp"
+#include "athlas_mini/adc.hpp"
+
+int main(int argc, char **argv) {
+	
+	ros::init(argc,argv,"ATHLAS-mini started");
+
+	return 0;
+}
